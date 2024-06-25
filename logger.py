ac357fda74833c8a31ab107da03df0970f708840
Patrik Kernstock <patrik@kernstock.net>
2024-06-25 02:57:27 +0200

    Improve timezone handling, use native python funcs

--- logger.py
+++ logger.py
@@ -2,17 +2,6 @@
 from datetime import datetime
 from os import environ
 
-# Only check local timezone if not running in a container, otherwise use UTC
-# Check if tzlocal is available, otherwise use pytz as a fallback with UTC
-try:
-    import tzlocal
-
-    local_timezone = tzlocal.get_localzone()
-except ImportError:
-    import pytz
-
-    local_timezone = pytz.timezone('UTC')
-
 # Constants
 debug = environ.get('CLEANUP_SCRIPT_DEBUG', 'false').lower() == 'true'
 
@@ -27,7 +16,7 @@ def log(sev: str, msg: str) -> None:
     severity = sev.upper()
     if not debug and severity == 'DEBUG':
         return
-    date = datetime.now(tz=local_timezone).strftime('%Y-%m-%dT%H:%M:%S%z')
+    date = datetime.now().astimezone().strftime('%Y-%m-%dT%H:%M:%S%z')
     print('[{0}] [{1: <5s}] {2}'.format(date, severity, msg))
 
     if severity == 'ERROR':
